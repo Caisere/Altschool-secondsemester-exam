a8bfd99b7f2dd605bdc8cd1c9f1e864b3d5a09fe
Omoshola <heylehgbedey@gmail.com>
2025-06-11 05:24:14 -0700

    feat:Error-Boundary Implementation

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { ErrorBoundary } from 'react-error-boundary'
+import ErrorBoundaryFallback from './pages/errorboundaryfallback'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <ErrorBoundary fallback={<ErrorBoundaryFallback />}>
+            <App />
+        </ErrorBoundary>
     </StrictMode>,
 )
---
+++ src/pages/errorboundaryfallback.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ErrorBoundaryFallback() {
+    return (
+        <div role="alert" className='flex flex-col items-center justify-center h-screen'>
+            <p className='text-2xl font-bold'>Oops! Something went wrong</p>
+            {/* <button className='bg-blue-500 text-white p-2 rounded-md' onClick={() => window.location.reload()}>Reload</button> */}
+            {/* <Link to='/'>Go to Home</Link> */}
+        </div>
+    );
+}
+
+export default ErrorBoundaryFallback;
